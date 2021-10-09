ff3f91d2b7208a3954cc40ef18830b75a91bc4e3
Colby Boell <68253967+cmboell@users.noreply.github.com>
2021-10-09 11:03:53 -0500

    Create editPetServlet.java
    added edit pet servlet

---
+++ src/controller/editPetServlet.java
@@ -0,0 +1,49 @@
+package controller;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Pet;
+
+/**
+ * Servlet implementation class addPetServlet
+ */
+@WebServlet("/addPetServlet")
+public class editPetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public editPetServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+       PetHelper dao = new PetHelper();
+		
+		String petName = request.getParameter("petName");
+		String petType = request.getParameter("petType");
+		Integer tempId = Integer.parseInt(request.getParameter("id"));
+				
+		Pet itemToUpdate = dao.searchForPetById(tempId);
+		itemToUpdate.setPetName(petName);
+		itemToUpdate.setPetType(petType);
+				
+		dao.updatePet(itemToUpdate);
+
+		getServletContext().getRequestDispatcher("/viewAllPetsServlet").forward(request, response);
+
+	}
+
+}
